ee37f561bdc9145f709efb55ca75087493899649
itomti <tomasivaskevic@gmail.com>
2020-12-03 08:48:09 +0200

    init

---
+++ Client/Services/BananaService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BlazorWasm.Client.Services
+{
+  public class BananaService : IBananaService
+  {
+    public event Action OnChange;
+    public int Bananas { get; set; } = 1000;
+
+    public void EatBananas(int amount)
+    {
+      Bananas -= amount;
+      BananasChanged();
+    }
+
+    void BananasChanged() => OnChange.Invoke();
+  }
+}
---
+++ Client/Services/IBananaService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BlazorWasm.Client.Services
+{
+  public interface IBananaService
+  {
+    event Action OnChange;
+    int Bananas { get; set; }
+
+    void EatBananas(int amount);
+  }
+}
